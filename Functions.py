bb3d25fcf7f11934de728de93de6fb5ea36d2697
Giacomo <giac290595@gmail.com>
2018-08-08 15:24:36 +0200

    added various functions added interface menu

--- Functions.py
+++ Functions.py
@@ -1,10 +1,41 @@
+import math
 
 # common statistical functions
 
 
-def mean(values):
+def average(values):
     dividend = 0
     for i in range(0, len(values)):
-        sum += values[i]
-    mean = sum/(len(values))
-    return mean
+        dividend += values[i]
+    avg = dividend/(len(values))
+    return avg
+
+
+def median(values):
+    return values[int(len(values)/2)]
+
+
+def trend(values):
+    the_most_frequent = 0
+    for value in values:
+        if values.count(value) > the_most_frequent:
+            the_most_frequent = value
+    return the_most_frequent
+
+
+def distance(val, avg):
+    dist = val - avg
+    return dist**2
+
+
+def std_deviation(values):                # mean squared error
+    dividend = 0
+    for value in values:
+        dividend += distance(value, average(values))
+    to_square = dividend/(len(values)-1)
+    return math.sqrt(to_square)
+
+
+
+
+
--- Interface.py
+++ Interface.py
@@ -1,5 +1,45 @@
 from  Functions import *
 
-vect = [1,3,9]
+vect = [1, 4, 12]
 
-print(mean(vect))
+
+def common_stats(values):
+    print("Average:  ", (average(values)))
+    print("Median: ", median(values))
+    print("Trend:  ", trend(values))
+    print("Standard Deviation:  ", std_deviation(values))
+
+
+menu = "Welcome to your scientific utility tool:\n" \
+       "Which operation do you need?\n\n" \
+       "a.   I want to get common statistical values of my vector\n" \
+       "b.   I want to predict object velocity\n"
+
+
+def main():
+    choice = input(menu)
+    if choice == "a":
+        choice_1 = input("Do you want to manually upload values or read from file?\n file / man\n")
+        if choice_1 == "man":
+            print("Insert value then press enter, press 'end' to end the vector\n")
+            value = 0
+            man_vect = []
+            vect = []
+            while value != "end":
+                value = input()
+                man_vect.append(value)
+            man_vect.remove("end")
+            for el in range(0, len(man_vect)):
+                vect.append(float(man_vect[el]))
+            common_stats(vect)
+        else:
+            file_location = input("Specify file location: the file has to be like this:\n\n"
+                                  "1 5 22 5.6 ......... 9.54\n")
+            file = open(file_location, "r")
+            file_vect = file.read().split()
+            vals =[]
+            for el in range(0, len(file_vect)):
+                vals.append(float(file_vect[el]))
+            common_stats(vals)
+
+main()
